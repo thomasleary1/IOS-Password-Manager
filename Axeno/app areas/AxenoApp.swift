6d65eb56f29b623bf1c6696bc9ea2629afc18014
Tom <67720817+thomasleary1@users.noreply.github.com>
2025-01-11 01:59:35 +1100

    Create AxenoApp.swift

---
+++ Axeno/app areas/AxenoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AxenoApp.swift
+//  Axeno
+//
+//  Created by Thomas Leary on 22/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct AxenoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
+    }
+}
